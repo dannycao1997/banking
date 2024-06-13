ae94fede110707b5deb60a18683695823551a9cc
Danny Cao <dannycao1997@yahoo.com>
2024-06-13 14:49:21 -0400

    save progress

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
+    const loggedIn = { firstName: 'Danny'};
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account
+                        transactions efficiently. "
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
